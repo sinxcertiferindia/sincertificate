17b0c1451e19cab2b819a80fd603b5295cffd9b9
manasparihar <kotharimanas71@gmail.com>
2025-12-19 20:58:52 +0530

    Fix Netlify function timeout by caching MongoDB connection

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const serverless = require("serverless-http");
-require("dotenv").config();
 
 const certificateRoutes = require("./routes/certificateRoutes");
 
@@ -10,19 +9,26 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-let isConnected = false;
+let cached = global.mongoose;
 
-async function connectDB() {
-  if (isConnected) return;
-  await mongoose.connect(process.env.MONGO_URI);
-  isConnected = true;
-  console.log("MongoDB connected");
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
-app.use(async (req, res, next) => {
-  await connectDB();
-  next();
-});
+async function connectDB() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.MONGO_URI).then((mongoose) => {
+      return mongoose;
+    });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+connectDB().then(() => console.log("MongoDB connected"));
 
 app.use("/api/certificates", certificateRoutes);
 
